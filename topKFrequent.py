9e9ed06faf79de65657a0da590403b98971cc205
Edward Chung <edwardchung2024@gmail.com>
2023-08-29 02:09:59 -0400

    topKupdate

--- topKFrequent.py
+++ topKFrequent.py
@@ -35,14 +35,37 @@ class Solution:
         #             return res
 
         # Solution 2: O(nlogn)
-        count = {}
+        # count = {}
+
+        # for n in nums:
+        #     count[n] = count.get(n, 0) + 1
+
+        # # Sort items in 'count' by their frequency in descending order and get the top k numbers
+        # sorted_items = sorted(count.items(), key=lambda item: item[1], reverse=True)
+        # return [item[0] for item in sorted_items[:k]]
+    
+        # Solution 2.0: Same as above but more intuitive solution
+        map1 = {}
 
         for n in nums:
-            count[n] = count.get(n, 0) + 1
+            map1[n] = map1.get(n, 0) + 1
 
-        # Sort items in 'count' by their frequency in descending order and get the top k numbers
-        sorted_items = sorted(count.items(), key=lambda item: item[1], reverse=True)
-        return [item[0] for item in sorted_items[:k]]
+        # sortedMap first becomes array of tuples -> [(1,3), (2,2), (3,1)] 
+        # dict cast makes it a dict -> {1:3, 2:2, 3:1}
+        # sortedMap.items() -> back to array of tuples -> [(1,3), (2,2), (3,1)] 
+        sortedMap = dict(sorted(map1.items(), key=lambda x: x[1], reverse=True))
+
+        ans = []
+        for key, val in sortedMap.items():
+            if len(ans) < k:
+                ans.append(key)
+
+        # for n in nums:
+        # for i in range(len(nums)):
+        # for i, n in enumerate(nums):
+        # for k, v in map1.items():
+
+        return ans
 
 newSolution = Solution()
 print(newSolution.topKFrequent([1,1,1,2,2,3,3,3,3,2,5,6,7,1,3,1,2,8,9], 3))
