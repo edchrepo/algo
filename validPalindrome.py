bfb350876f4983222269990ede197365dbb3e0e5
Edward Chung <edwardchung2024@gmail.com>
2023-09-12 18:11:08 -0400

    validPalindrome

--- validPalindrome.py
+++ validPalindrome.py
@@ -23,11 +23,36 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         newString = ''.join([char for char in s if char.isalnum()]).lower()
 
-        for i in range(len(newString) // 2):
-            if newString[i] != newString[len(newString) - i - 1]:
-                return False
+        # Solution 0: reverse method
+        # return newString[::-1] == newString
+
+        # Solution 1 O(n): loop halfway and check start and end indices to see if they match, keep incrementing after
+        # for i in range(len(newString) // 2):
+        #     if newString[i] != newString[len(newString) - i - 1]:
+        #         return False
             
+        # return True
+
+    # Solution 2 O(n) but O(1) memory: Two pointers
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True
+
+    # Could write own alpha-numeric function
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
     
 newSolution = Solution()
 print(newSolution.isPalindrome("A man, a plan, a canal: Panama"))
