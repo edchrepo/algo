b82adff12b42be0e47c895ce3121ec9f1b20b7c4
Edward Chung <edwardchung2024@gmail.com>
2023-10-02 17:52:52 -0400

    mostwater

--- containerMostWater.py
+++ containerMostWater.py
@@ -8,14 +8,13 @@ class Solution:
 
         l, r = 0, len(height) - 1
         while l < r:
+            area = min(height[l], height[r])*(r-l)
+            maxArea = max(area, maxArea)
+
             if height[r] > height[l]:
                 l += 1
             elif height[l] > height[r]:
                 r -= 1
-            area = min(height[l], height[r])*(r-l)
-            maxArea = max(area, maxArea)
-            l += 1
-            r -= 1
 
         return maxArea
 
